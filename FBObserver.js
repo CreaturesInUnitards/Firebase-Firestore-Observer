b824b334cbf70dcc59fbec24cff33761da6306fe
Scotty Simpson <portrayme@mac.com>
2018-02-02 06:34:17 -0800

    Update FBObserver.js

--- FBObserver.js
+++ FBObserver.js
@@ -1,13 +1,11 @@
-;(() => {
-    const FBObserve = (collectionName, crudFn, redrawFn) => firebase.firestore().collection(collectionName)
-        .onSnapshot((snap) => {
-            snap.docChanges.forEach((change) => {
-                (new Promise((resolve) => { crudFn(change).then(resolve) }))
-                .then(redrawFn).catch((e) => console.log(e, 'Your CRUD function must return a promise.'))
-            })
+const FBObserve = (collectionName, crudFn, redrawFn) => firebase.firestore().collection(collectionName)
+    .onSnapshot((snap) => {
+        snap.docChanges.forEach((change) => {
+            (new Promise((resolve) => { crudFn(change).then(resolve) }))
+            .then(redrawFn).catch((e) => console.log(e, 'Your CRUD function must return a promise.'))
         })
+    })
 
-    if (typeof module === 'object') module.exports = FBObserve
-    else if (typeof window !== 'undefined') window.FBObserve = FBObserve
-    else global.FBObserve = FBObserve
-})()
+if (typeof module === 'object') module.exports = FBObserve
+else if (typeof window !== 'undefined') window.FBObserve = FBObserve
+else global.FBObserve = FBObserve
